089c65ef6c74e3629dd7d23516dcdae12cb3584b
Damian Panes <damkancontacto@gmail.com>
2022-05-09 18:58:44 -0400

    commit inicial en donde se añaden las ventanas loading y launcher

---
+++ index.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow,Menu, ipcMain } = require('electron')
+const url = require('url')
+const path = require('path')
+
+app.on('ready', () => {
+    loadingScreen = new BrowserWindow({
+        
+    })
+})
